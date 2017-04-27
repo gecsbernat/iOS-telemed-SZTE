4c00d9838f98b7afddd1b82bbc7c9e7af99d6cbf
Student <student@imac07-1.inf.u-szeged.hu>
2017-04-27 10:59:00 +0200

    új

---
+++ ios-telemed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-telemed
+//
+//  Created by Student on 2017. 04. 27..
+//  Copyright © 2017. ios2017. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
