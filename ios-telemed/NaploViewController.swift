fb2bbdf415229315734da17c01c134e3806d58dd
Student <student@imac07-1.inf.u-szeged.hu>
2017-04-27 11:23:54 +0200

    salami

---
+++ ios-telemed/NaploViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NaploViewController.swift
+//  ios-telemed
+//
+//  Created by Student on 2017. 04. 27..
+//  Copyright © 2017. ios2017. All rights reserved.
+//
+
+import UIKit
+
+class NaploViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
