839c3350a85367e4672c4f5e249caa204eec0184
Rita Sumegi <ritasumegi@Rita-MacBook-Pro.local>
2017-05-09 17:57:21 +0200

    idopontok elkezdve

---
+++ IdopontokViewController.swift
@@ -0,0 +1,35 @@
+//
+//  IdopontokViewController.swift
+//  ios-telemed
+//
+//  Created by Rita Sumegi on 2017. 05. 09..
+//  Copyright © 2017. ios2017. All rights reserved.
+//
+
+import UIKit
+
+class IdopontokViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
